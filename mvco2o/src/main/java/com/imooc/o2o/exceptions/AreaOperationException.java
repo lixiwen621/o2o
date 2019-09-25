a28541d7c22d6904f2390ab4ea6ef47bf9c04e4b
xiwen <15922697280@163.com>
2019-09-25 17:21:26 +0800

    添加o2o

---
+++ mvco2o/src/main/java/com/imooc/o2o/exceptions/AreaOperationException.java
@@ -0,0 +1,13 @@
+package com.imooc.o2o.exceptions;
+
+public class AreaOperationException extends RuntimeException {
+
+	/**
+	 * 	继承的各类异常
+	 */
+	private static final long serialVersionUID = -1512771573934050550L;
+
+	public AreaOperationException(String msg) {
+		super(msg);
+	}
+}
